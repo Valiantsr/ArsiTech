02083e6c86ef8b65440613371328da2ed6922e97
valliant <valiant.shabri@gmail.com>
2020-12-15 10:46:40 +0700

    --

--- resources/views/layouts/myView.blade.php
+++ resources/views/layouts/myView.blade.php
@@ -75,7 +75,7 @@
         <!-- ./wrapper -->
 
         <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
-        <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        {{-- <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script> --}}
         <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
         <script src="{{ asset('plugins/toastr/toastr.min.js') }}"></script>
         <script src="{{ asset('dist/js/demo.js') }}"></script>
